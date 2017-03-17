a1f3495d42d8f2cd9cab50bf940b1eba2860d64a
Sai Uday Bhaskar Mudivarty <narenuday@Sais-MacBook-Pro.local>
2017-03-16 18:18:25 -0700

    Selection Sort using Arrays

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,8 +1,30 @@
+#!/bin/python3
+'''
+Author: Sai Uday Bhaskar Mudivarthy
+Program: Selection Sort
+Time Complexity: O(n*n)
+'''
+
 import sys
-n = raw_input().strip().split(' ')
-n = list(map(int, n))
-x=0
-for i in n:
-    x = x+i
-print(x)
+from array import *
+
+def swap(intarray,i,imin):
+    temp = intarray[i]
+    intarray[i]=intarray[imin]
+    intarray[imin]=temp
+
+
+n =  input().strip().split(' ')
+intarray = array('i',list(map(int,n)))
+x = 0
+for i in range(len(intarray)-1):
+    iMin = i
+    for j in range(i+1,len(intarray)):
+        if intarray[j]<intarray[iMin]:
+            iMin = j
+    swap(intarray,i,iMin)
+
+for i in intarray:
+    print(i)
+
 

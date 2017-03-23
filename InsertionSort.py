2a18bcf3f7ee976e0954390743717f11b9eba292
Sai Uday Bhaskar Mudivarty <narenuday@Sais-MacBook-Pro.local>
2017-03-23 14:44:11 -0700

    Insertion Sort Illustration

--- InsertionSort.py
+++ InsertionSort.py
@@ -8,16 +8,15 @@ Worst Case, Average Case: O(n*n)
 '''
 import sys
 from array import *
-
-
-n =  input().strip().split(' ')
-intarray = array('i',list(map(int,n)))
-isMoved = False
-print()
-print("Insertion Sort works this way with every itteration by making two subsets Sorted and Unsorted")
-for i in range(1,len(intarray)):
-    x=intarray[i]
-    j= i-1
+def illustrateSortItteration(intarray,i,j):
+    for k in range(i+1):
+        if k == j:
+            print("X",end=" ")
+        else:
+            print(intarray[k],end=" ")
+    print()
+    
+def printSortedUnsortedSubsets(intarray,i):
     print()
     print("Sorted Subset",end=": ")
     for k in range(i):
@@ -27,25 +26,27 @@ for i in range(1,len(intarray)):
         print(intarray[k],end=" ")
     print()
     print("Finding the perfect position for "+str(x)+" in Sorted Array. We will insert "+str(x)+" inplace of X")
+
+n =  input().strip().split(' ')
+intarray = array('i',list(map(int,n)))
+isMoved = False
+print("Insertion Sort works this way with every itteration by making two subsets Sorted and Unsorted")
+for i in range(1,len(intarray)):
+    x=intarray[i]
+    j= i-1
+    printSortedUnsortedSubsets(intarray,i)
     while j>=0 and intarray[j]>x:
         isMoved = True
+        illustrateSortItteration(intarray,i,j)
         intarray[j+1]=intarray[j]
-        
-        for k in range(i+1):
-            if k == j:
-                print("X",end=" ")
-            else:
-                print(intarray[k],end=" ")
         j = j-1
-        print()
     intarray[j+1] = x
     if not isMoved:
         print("The element "+str(x)+" will be in the right position for the Sorted Array")
-        print()
         isMoved = False
 
 print()
-print("Sorted List Follows")
+print("Finally Sorted List: ",end=" ")
 for i in intarray:
     print(i,end=" ")
 print()
